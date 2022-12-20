686c786efb26f1880c888f3c6df8163d5cbca3b4
Muralitharan Alwar <muralitharan.alwar@gmail.com>
2022-12-20 19:44:21 +0800

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
   
   /** UPDATE YOUR VERIFY TOKEN **/
-  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+  const VERIFY_TOKEN = "VT";
   
   // Parse params from the webhook verification request
   let mode = req.query['hub.mode'];
